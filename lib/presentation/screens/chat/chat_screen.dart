5525e06607e2a19262fa8ca1f56408c7077adece
Fabio Nelson Usuga Silva <fusuga@integral.com.co>
2023-05-29 15:24:55 -0500

    Seccion 5: elementos graficos e interaccion

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,52 @@
+//importM
+
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI9ifWgHEerOEYhVP20INWQxYDRZv_h4fCyw&usqp=CAU')),
+        ),
+        title: const Text('Comics chat'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return (index %2 ==0)
+                       ? const HerMessageBubble()
+                       : const MyMessageBubble();                                
+              },
+            )),
+            //Caja de texto de mensajes.
+            const MessageFieldBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
